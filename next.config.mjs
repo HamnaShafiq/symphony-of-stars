45ff6a7d9897354cb4648b88b27db3f395a79ed2
HamnaShafiq <hamnashafiq012@gmail.com>
2024-10-07 23:05:25 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Enables static export for GitHub Pages
+  assetPrefix: '/symphony-of-stars/', // Add your repo name here
+  images: {
+    unoptimized: true // Disable image optimization for static export
+  }
 };
 
 export default nextConfig;
